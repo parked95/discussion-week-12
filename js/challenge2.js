3e53505c290507816d017979abfce621aedb1525
David Parker <143548413+parked95@users.noreply.github.com>
2024-04-04 15:52:06 -0400

    Add files via upload

---
+++ js/challenge2.js
@@ -0,0 +1,11 @@
+document.querySelector("#sameAddress").addEventListener("click", function() {
+    let bill = document.querySelector("#bill");
+    let home = document.querySelector("#home");
+
+    if(this.checked) {
+        home.value = bill.value;
+        home.disabled = true;
+    } else {
+        home.value = "";
+        home.disabled = false;
+    });
